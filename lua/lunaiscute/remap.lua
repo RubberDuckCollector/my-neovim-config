514df3afcd5d8b2bea3025a4ccb36637895a57bd
Luna Long <eggsim49@gmail.com>
2024-02-26 23:01:16 +0000

    new mapping

--- lua/lunaiscute/remap.lua
+++ lua/lunaiscute/remap.lua
@@ -35,3 +35,9 @@ vim.keymap.set("v", "<leader>d", '"_d')
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
+
+vim.keymap.set(
+  "n",
+  "<leader>t",
+  "_ithe <Esc>"
+)
