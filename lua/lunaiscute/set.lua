0d34b332716c75b41c090564448a45db22b9735f
Luna Long <eggsim49@gmail.com>
2024-03-08 16:46:56 +0000

    set.lua

--- lua/lunaiscute/set.lua
+++ lua/lunaiscute/set.lua
@@ -18,9 +18,13 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.scrolloff = 1000 -- cursor will stay 1000 lines from the bottom/top when scrolling down
-vim.opt.cmdheight = 2    -- more space in the neovim command line for displaying messages
-vim.opt.wrap = false     -- no soft wrap
+-- with these 2 settings, the cursor will always stay in the middle of the screen where possible.
+-- I like this because it's great for readability when i'm writing long lines of documentation in .md files.
+vim.opt.scrolloff = 1000     -- cursor will stay 1000 lines from the bottom/top when scrolling down
+vim.opt.sidescrolloff = 1000 -- cursor will stay 1000 lines from the left/right when side-scrolling
+
+vim.opt.cmdheight = 2        -- more space in the neovim command line for displaying messages
+vim.opt.wrap = false         -- no soft wrap
 
 -- search results highlight themselves reactively
 vim.opt.hlsearch = false
@@ -37,25 +41,25 @@ vim.opt.cursorcolumn = true
 vim.opt.history = 1000
 
 function Set_filetype_settings()
-    local concerned_files = { "lua", "c", "cpp", "rb", "haskell" }
-    local filetype = vim.bo.filetype
+  local concerned_files = { "lua", "c", "cpp", "rb", "haskell" }
+  local filetype = vim.bo.filetype
 
-    local found = false
-    for _, lang in ipairs(concerned_files) do
-        if lang == filetype then
-            found = true
-            break
-        end
+  local found = false
+  for _, lang in ipairs(concerned_files) do
+    if lang == filetype then
+      found = true
+      break
     end
+  end
 
-    if found then
-        vim.bo.tabstop = 2
-        vim.bo.softtabstop = 2
-        vim.bo.shiftwidth = 2
-    else
-        do
-        end
+  if found then
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.shiftwidth = 2
+  else
+    do
     end
+  end
 end
 
 -- Set autocmd for FileType event to trigger the function
