913461444a768ec4636f8f25115ddb28a5da66c5
RubberDuckCollector <eggsim49@gmail.com>
2025-12-26 02:19:39 +0000

    update lspconfig to not be deprecated

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,41 +1,14 @@
--- Setup language servers.
-local lspconfig = require('lspconfig')
--- lspconfig.pylsp.setup({
---   settings = {
---     pylsp = {
---       plugins = {
---         flake8 = {
---           extendIgnore = { "E501" }
---         }
---       }
---     }
---   }
--- })
--- lspconfig.ltex.setup {
---   settings = {
---     ltex = {
---       language = { "es", "en-GB" },
---     }
---   }
--- }
-lspconfig.pyright.setup {}
-lspconfig.ocamllsp.setup {}
-lspconfig.clangd.setup {}
-lspconfig.omnisharp.setup {}
-lspconfig.bashls.setup {}
-lspconfig.ts_ls.setup {}
-lspconfig.clojure_lsp.setup {}
-lspconfig.lua_ls.setup {}
-lspconfig.gopls.setup {}
-lspconfig.solargraph.setup {}
-lspconfig.zls.setup {}
-lspconfig.gleam.setup {}
-lspconfig.texlab.setup {}
-lspconfig.rust_analyzer.setup {
-  -- Server-specific settings. See `:help lspconfig-setup`
-  settings = {
-    ['rust-analyzer'] = {},
-  },
-}
-
-return lspconfig
+vim.lsp.enable('clangd')
+vim.lsp.enable('pyright')
+vim.lsp.enable('ocamllsp')
+vim.lsp.enable('omnisharp')
+vim.lsp.enable('bashls')
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('clojure_lsp')
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('gopls')
+vim.lsp.enable('solargraph')
+vim.lsp.enable('zls')
+vim.lsp.enable('gleam')
+vim.lsp.enable('texlab')
+vim.lsp.enable('rust_analyzer')
