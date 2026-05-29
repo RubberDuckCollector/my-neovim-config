a681da32739a776028d94671a30365c064c81f1c
RubberDuckCollector <eggsim49@gmail.com>
2026-05-29 23:02:07 +0100

    init.lua

--- init.lua
+++ init.lua
@@ -662,22 +662,39 @@ do
   vim.keymap.set('n', '<leader>vs', '<cmd>Love stop<cr>',   { desc = 'Stop LÖVE' })
   vim.keymap.set('n', '<leader>vo', '<cmd>Love output<cr>', { desc = 'Output panel' })
 
-  vim.pack.add { gh 'yousefhadder/markdown-plus.nvim' }
-  require("markdown-plus").setup({})
-
-  -- vim.pack.add { gh 'tadmccorkle/markdown.nvim' }
-  -- require('markdown').setup({
-  --   mappings = {},
-  --   inline_surround = {},
-  --   toc = {},
-  --   hooks = {
-  --     -- Called when following links. Provided the following options:
-  --     -- * 'dest' (string): the link destination
-  --     -- * 'use_default_app' (boolean|nil): whether to open the destination with default application
-  --     --   (refer to documentation on <Plug> mappings for explanation of when this option is used)
-  --     follow_link = nil,
-  --   },
-  -- })
+  vim.pack.add { gh 'wallpants/github-preview.nvim' } -- BUN IS A DEPENDENCY
+  require("github-preview").setup({
+    host = "localhost",
+    -- port used by local server
+    port = 6041,
+    -- set to "true" to force single-file mode & disable repository mode
+    single_file = false,
+    theme = {
+      -- "system" | "light" | "dark"
+      name = "system",
+      high_contrast = false,
+    },
+    -- define how to render <details> tags on init/content-change
+    -- true: <details> tags are rendered open
+    -- false: <details> tags are rendered closed
+    details_tags_open = true,
+    cursor_line = {
+      disable = false,
+      -- CSS color
+      -- if you provide an invalid value, cursorline will be invisible
+      color = "#c86414",
+      opacity = 0.2,
+    },
+    scroll = {
+      disable = false,
+      -- Between 0 and 100
+      -- VERY LOW and VERY HIGH numbers might result in cursorline out of screen
+      top_offset_pct = 35,
+    },
+    -- for debugging
+    -- nil | "debug" | "verbose"
+    log_level = nil,
+  })
 
   vim.pack.add { gh 'chomosuke/typst-preview.nvim' }
   -- vim.pack.add { gh 'junegunn/seoul256.vim' }
@@ -687,13 +704,39 @@ do
   require('onedark').load()
   vim.pack.add { gh 'alanfortlink/blackjack.nvim' }
   vim.pack.add { gh 'dhruvasagar/vim-table-mode' }
-  vim.pack.add { gh 'lukas-reineke/indent-blankline.nvim' }
+  -- vim.pack.add { gh 'lukas-reineke/indent-blankline.nvim' }
   -- return 'ibl'.setup({})
   vim.pack.add { gh 'mzlogin/vim-markdown-toc' }
   vim.pack.add { gh 'mbbill/undotree' }
-  vim.pack.add { gh 'iamcco/markdown-preview.nvim' }
   vim.pack.add { gh 'Raimondi/delimitMate.git' }
+  vim.pack.add { gh 'jbyuki/venn.nvim' }
+  -- venn.nvim: enable or disable keymappings
+  function _G.Toggle_venn()
+    local venn_enabled = vim.inspect(vim.b.venn_enabled)
+    if venn_enabled == "nil" then
+      vim.b.venn_enabled = true
+      vim.cmd[[setlocal ve=all]]
+      -- draw a line on HJKL keystokes
+      vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", {noremap = true})
+      vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", {noremap = true})
+      vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", {noremap = true})
+      vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", {noremap = true})
+      -- draw a box by pressing "f" with visual selection
+      vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", {noremap = true})
+    else
+      vim.cmd[[setlocal ve=]]
+      vim.api.nvim_buf_del_keymap(0, "n", "J")
+      vim.api.nvim_buf_del_keymap(0, "n", "K")
+      vim.api.nvim_buf_del_keymap(0, "n", "L")
+      vim.api.nvim_buf_del_keymap(0, "n", "H")
+      vim.api.nvim_buf_del_keymap(0, "v", "f")
+      vim.b.venn_enabled = nil
+    end
+  end
+  -- toggle keymappings for venn using <leader>v
+  vim.api.nvim_set_keymap('n', '<leader>v', ":lua Toggle_venn()<CR>", { noremap = true})
 
+  -- vim.pack.add { gh 'saghen/filler-begone.nvim' }
 
   -- Simple and easy statusline.
   --  You could remove this setup call if you don't like it,
@@ -710,6 +753,18 @@ do
 
   -- ... and there is more!
   --  Check out: https://github.com/nvim-mini/mini.nvim
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function()
+      -- load the optional plugin
+      vim.pack.add { gh 'yousefhadder/markdown-plus.nvim' }
+      -- then run the plugin setup if needed
+      if package.loaded["markdown-plus"] == nil then
+        require("markdown-plus").setup()
+      end
+    end,
+  })
 end
 
 -- ============================================================
@@ -991,6 +1046,8 @@ do
         },
       },
     },
+
+    omnisharp = {},
   }
 
   vim.pack.add {
